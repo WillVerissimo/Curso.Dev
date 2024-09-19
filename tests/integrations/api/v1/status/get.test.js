ceab2738c22fb9dfc0c0e15b3979ec676dd85749
Willian Veríssimo <168719237+WillVerissimo@users.noreply.github.com>
2024-09-19 05:23:45 +0000

    Fixing some issues

--- tests/integrations/api/v1/status/get.test.js
+++ tests/integrations/api/v1/status/get.test.js
@@ -7,7 +7,7 @@ test("GET to api/v1/status should return 200", async () => {
   const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parseUpdatedAt);
 
-  expect(responseBody.dependencies.database.version).toEqual("16.4");
-  expect(responseBody.dependencies.database.max_connections).toEqual(112);
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
